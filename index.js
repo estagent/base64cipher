746599c3a3a18bed34b159cbb51f4f88393c39b3
estagent <estagent@gmail.com>
2022-03-15 04:17:11 +0300

    initial

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 export default {
-    validateSecret: secret => typeof secret === 'string' && secret.length > 16,
-    validateExpiry: expiresAt =>
-        typeof expiresAt === 'number' && expiresAt > Date.now(),
-    decodeCredentials: credentials =>
-        typeof credentials === 'string' ? JSON.parse(atob(credentials)) : null,
-    encodeCredentials: data =>
-        typeof data === 'object' ? btoa(JSON.stringify(data)) : null,
-    validate(data) {
-        this.validateSecret(data.secret) && this.validateExpiry(data.expiresAt)
-    },
+  validateSecret: secret => typeof secret === 'string' && secret.length > 16,
+  validateExpiry: expiresAt =>
+    typeof expiresAt === 'number' && expiresAt > Date.now(),
+  decodeCredentials: credentials =>
+    typeof credentials === 'string' ? JSON.parse(atob(credentials)) : null,
+  encodeCredentials: data =>
+    typeof data === 'object' ? btoa(JSON.stringify(data)) : null,
+  validate(data) {
+    return (
+      this.validateSecret(data.secret) && this.validateExpiry(data.expiresAt)
+    )
+  },
 }
